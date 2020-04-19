110148d09ddca67995b102ea807ce6b5ca434e61
Musigwa <pacifique.musigwa@gmail.com>
2020-04-19 12:40:36 +0200

    add the status to the errorHandler

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,15 +1,15 @@
-const passport = require("passport");
-const { statusCodes, errorMessages } = require("../constants");
-const async = require("./errorHandler");
+const passport = require('passport');
+const { statusCodes, errorMessages } = require('../constants');
+const async = require('./errorHandler');
 
 module.exports = async((req, res, next) => {
-  return passport.authenticate("jwt", { session: false }, (error, user) => {
-    if (error) throw new Error(errorMessages.UNAUTHORIZED);
-    if (!user)
-      {return res.status(statusCodes.UNAUTHORIZED).json({
-        message: errorMessages.UNAUTHORIZED,
-      });}
-    req.user = user;
-    return next();
+  return passport.authenticate('jwt', { session: false }, (error, user) => {
+    let error = new Error(errorMessages.UNAUTHORIZED);
+    error.status = statusCodes.UNAUTHORIZED;
+    if (error || !user) throw error;
+    else {
+      req.user = user;
+      return next();
+    }
   })(req, res, next);
 });
